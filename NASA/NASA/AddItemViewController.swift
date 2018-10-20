1cacb9371237e66818ed72a4f1ac2327bb4377e6
misomiso49 <misomiso43@hotmail.co>
2018-10-20 15:03:05 +0800

    add two pages

---
+++ NASA/NASA/AddItemViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddItemViewController.swift
+//  NASA
+//
+//  Created by 鄭羽辰 on 2018/10/20.
+//  Copyright © 2018 鄭羽辰. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+    var infofromview4:Int?
+    
+    @IBAction func textInputButton(_ sender: UIButton) {
+        
+    }
+    @IBOutlet weak var itemInputTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
